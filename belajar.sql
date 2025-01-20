b9d379865441765d62173d3141c9580a12a602cb
erikaufa <erikaptrfadluna08.gmail.com>
2025-01-20 09:47:47 +0700

    database update

--- belajar.sql
+++ belajar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 16, 2025 at 03:06 AM
+-- Generation Time: Jan 20, 2025 at 02:47 AM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `data_biaya` (
   `id` int NOT NULL,
-  `jenis_biaya` varchar(100) NOT NULL,
-  `keterangan` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nama_biaya` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `deskripsi` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_at` date NOT NULL,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` int NOT NULL
@@ -40,25 +40,36 @@ CREATE TABLE `data_biaya` (
 -- Dumping data for table `data_biaya`
 --
 
-INSERT INTO `data_biaya` (`id`, `jenis_biaya`, `keterangan`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Reguler', 'blabl', '2025-01-16', '2025-01-16 03:02:48', 0);
+INSERT INTO `data_biaya` (`id`, `nama_biaya`, `deskripsi`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(19, 'seragam', 'blablaba', '2025-01-19', '2025-01-19 14:14:00', 0),
+(20, 'kiw', 'blablabala', '2025-01-18', '2025-01-18 04:49:32', 0),
+(21, 'minum', 'blablabala', '2025-01-19', '2025-01-19 15:45:24', 0),
+(22, 'makan', 'blabl', '2025-01-19', '2025-01-19 16:20:27', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data_harga`
+-- Table structure for table `data_harga_biaya`
 --
 
-CREATE TABLE `data_harga` (
+CREATE TABLE `data_harga_biaya` (
   `id` int NOT NULL,
   `id_tahun_pelajaran` int NOT NULL,
-  `id_kelas` int NOT NULL,
-  `id_jenis_biaya` int NOT NULL,
+  `id_biaya` int NOT NULL,
   `harga` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_at` date NOT NULL,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data_harga_biaya`
+--
+
+INSERT INTO `data_harga_biaya` (`id`, `id_tahun_pelajaran`, `id_biaya`, `harga`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(12, 2, 19, '22', '2025-01-20', '2025-01-20 02:40:15', 0),
+(13, 2, 20, '23', '2025-01-19', '2025-01-19 15:01:48', 0),
+(14, 2, 19, '300000', '2025-01-19', '2025-01-19 15:27:08', 0);
 
 -- --------------------------------------------------------
 
@@ -80,9 +91,10 @@ CREATE TABLE `data_jurusan` (
 --
 
 INSERT INTO `data_jurusan` (`id`, `id_tahun_pelajaran`, `nama_jurusan`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 'DKV', '2025-01-15 03:40:06', '2025-01-15 03:40:06', 0),
+(1, 1, 'DKV', '2025-01-15 03:40:06', '2025-01-15 03:40:06', 1737188372),
 (2, 2, 'RPL', '2025-01-15 06:54:44', '2025-01-15 06:54:44', 0),
-(3, 3, 'TKJ', '2025-01-15 06:54:58', '2025-01-15 06:54:58', 0);
+(3, 3, 'TKJ', '2025-01-15 06:54:58', '2025-01-15 06:54:58', 0),
+(4, 2, 'DKV', '2025-01-18 08:20:23', '2025-01-18 08:20:23', 0);
 
 -- --------------------------------------------------------
 
@@ -105,9 +117,10 @@ CREATE TABLE `data_kelas` (
 --
 
 INSERT INTO `data_kelas` (`id`, `id_tahun_pelajaran`, `id_jurusan`, `nama_kelas`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 0, 1, '10 DKV', '2025-01-15', '2025-01-15 06:55:35', 0),
+(1, 0, 1, '10 DKV', '2025-01-15', '2025-01-15 06:55:35', 1737187682),
 (2, 0, 2, '11 RPL', '2025-01-15', '2025-01-15 06:55:50', 0),
-(3, 0, 3, '10 TKJ', '2025-01-15', '2025-01-15 06:56:06', 0);
+(3, 0, 3, '10 TKJ', '2025-01-15', '2025-01-15 06:56:06', 0),
+(4, 0, 2, '12 RPL', '2025-01-18', '2025-01-18 07:56:05', 0);
 
 -- --------------------------------------------------------
 
@@ -128,8 +141,11 @@ CREATE TABLE `data_seragam` (
 --
 
 INSERT INTO `data_seragam` (`id`, `nama_seragam`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'putih ', '2025-01-15', '2025-01-15 18:55:34', 0),
-(2, 'batik', '2025-01-16', '2025-01-16 03:00:20', 0);
+(4, 'putih ', '2025-01-17', '2025-01-17 17:33:43', 0),
+(5, 'pdh', '2025-01-17', '2025-01-17 18:29:49', 0),
+(6, 'olahraga', '2025-01-17', '2025-01-17 18:54:27', 0),
+(7, 'putih abu', '2025-01-17', '2025-01-17 19:12:38', 0),
+(10, 'rompi', '2025-01-19', '2025-01-19 16:30:14', 0);
 
 -- --------------------------------------------------------
 
@@ -140,15 +156,22 @@ INSERT INTO `data_seragam` (`id`, `nama_seragam`, `created_at`, `updated_at`, `d
 CREATE TABLE `data_stok` (
   `id` int NOT NULL,
   `id_tahun_pelajaran` int NOT NULL,
-  `id_jurusan` int NOT NULL,
-  `id_kelas` int NOT NULL,
   `id_seragam` int NOT NULL,
   `ukuran` varchar(100) NOT NULL,
   `stok` int NOT NULL,
-  `ceated_at` date NOT NULL,
+  `created_at` date NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data_stok`
+--
+
+INSERT INTO `data_stok` (`id`, `id_tahun_pelajaran`, `id_seragam`, `ukuran`, `stok`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(5, 0, 5, 'S', 98, '2025-01-19', '2025-01-19 16:50:31', 0),
+(6, 3, 6, 'M', 15, '2025-01-20', '2025-01-20 02:40:42', 0),
+(7, 4, 4, 'S', 90, '2025-01-20', '2025-01-20 02:41:19', 0);
 
 -- --------------------------------------------------------
 
@@ -172,9 +195,10 @@ CREATE TABLE `data_tahun_pelajaran` (
 --
 
 INSERT INTO `data_tahun_pelajaran` (`id`, `nama_tahun_pelajaran`, `tanggal_mulai`, `tanggal_akhir`, `status_tahun_pelajaran`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, '2025-2026', '2025-01-15', '2026-01-15', '1', '2025-01-15 03:05:10', '2025-01-15 03:05:10', 0),
-(2, '2026-2027', '2025-01-01', '2026-01-01', '1', '2025-01-15 03:39:06', '2025-01-15 03:39:06', 0),
-(3, '2027-2028', '2025-01-01', '2026-01-01', '1', '2025-01-15 03:39:43', '2025-01-15 03:39:43', 0);
+(1, '2025-2026', '2025-01-15', '2026-01-15', '1', '2025-01-15 03:05:10', '2025-01-15 03:05:10', 1737189105),
+(2, '2026-2027', '2025-01-01', '2026-01-01', '0', '2025-01-19 14:30:21', '2025-01-19 14:30:21', 0),
+(3, '2027-2028', '2025-01-01', '2026-01-01', '1', '2025-01-15 03:39:43', '2025-01-15 03:39:43', 0),
+(4, '2025/2026', '2025-01-03', '2025-01-30', '1', '2025-01-18 08:32:01', '2025-01-18 08:32:01', 0);
 
 -- --------------------------------------------------------
 
@@ -209,9 +233,9 @@ ALTER TABLE `data_biaya`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `data_harga`
+-- Indexes for table `data_harga_biaya`
 --
-ALTER TABLE `data_harga`
+ALTER TABLE `data_harga_biaya`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -258,43 +282,43 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data_biaya`
 --
 ALTER TABLE `data_biaya`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT for table `data_harga`
+-- AUTO_INCREMENT for table `data_harga_biaya`
 --
-ALTER TABLE `data_harga`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `data_harga_biaya`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `data_jurusan`
 --
 ALTER TABLE `data_jurusan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `data_kelas`
 --
 ALTER TABLE `data_kelas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `data_seragam`
 --
 ALTER TABLE `data_seragam`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `data_stok`
 --
 ALTER TABLE `data_stok`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `data_tahun_pelajaran`
 --
 ALTER TABLE `data_tahun_pelajaran`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
